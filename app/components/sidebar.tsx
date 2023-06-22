f4b12e3793129c4add7312bb050331ce0ca49885
xX_Albert <49263337+burberrys@users.noreply.github.com>
2023-06-22 22:46:22 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
            <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <img src={https://work.weixin.qq.com/u/vce31f0fff64885a95?v=4.1.2.119220} alt="QR Code">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
