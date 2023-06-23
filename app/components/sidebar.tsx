028ccb270ebef8b8495935e4d147ad6e8293f31d
xX_Albert <49263337+burberrys@users.noreply.github.com>
2023-06-23 11:13:55 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,7 +5,7 @@ import styles from "./home.module.scss";
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
 import GithubIcon from "../icons/github.svg";
-import ChatGptIcon from "../icons/chatgpt.svg";
+import ChatGptIcon from "../icons/github.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
 import MaskIcon from "../icons/mask.svg";
@@ -127,7 +127,7 @@ export function SideBar(props: { className?: string }) {
           Your own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
-          <GithubIcon />
+          <ChatGptIcon />
         </div>
       </div>
 
